48cab10e5bd58e0e95e2f5757ddc791ca132b190
guditubagus <133938024+guditubagus@users.noreply.github.com>
2023-06-05 08:56:11 +0700

    add error handling not found paths

--- src-backend-marketplace/app.js
+++ src-backend-marketplace/app.js
@@ -12,5 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
